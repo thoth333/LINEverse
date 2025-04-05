fe35f18d395f03cd22aa4a2213121014f157941e
とーと <128669695+thoth333@users.noreply.github.com>
2025-04-05 11:35:29 +0000

    外形作成

--- app.vue
+++ app.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <img src="/img/Background.png" class="bg"/>
+  <div class="center">
+  <a>saa</a>
+  <p>txtをアップロード</p>
+  <p>自分のアイコンをアップロード</p>
+  <img src="/img/github-mark.svg" class="center"/>
+    <a>saa</a>  <!-- Don't forget to add URL-->
+</div>
 </template>
+
+
+<style>
+.bg {
+  object-fit: cover;
+  object-position: center center;
+  width: 100%;
+  height: 100%;
+}
+
+
+.center {
+  text-align: center;
+}
+</style>
