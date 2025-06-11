0f900a35ffbc644cf41bb2978a2a4c1dc975fb0e
とーと <hibiyahibiyahibiya77th@gmail.com>
2025-06-11 17:39:32 +0900

    feat: implement WPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@vite-pwa/nuxt'],
 	devtools: { enabled: true },
+	app: {
+		head: {
+			title: 'LINEverse',
+			htmlAttrs: { lang: 'ja' },
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			],
+		},
+	},
 	css: ['~/assets/css/main.css'],
 	compatibilityDate: '2024-11-01',
 	eslint: {
@@ -9,9 +18,9 @@ export default defineNuxtConfig({
 			stylistic: {
 				indent: 'tab',
 				semi: true,
-				// ...
 			},
-		} },
+		},
+	},
 	pwa: {
 		registerType: 'autoUpdate',
 		manifest: {
@@ -25,6 +34,10 @@ export default defineNuxtConfig({
 				{ src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
 				{ src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
 			],
+		},
+		workbox: {
+			navigateFallback: null,
+			// よく分かってない…
 		},
 	},
 });
